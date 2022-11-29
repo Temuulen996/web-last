5cf63e0584f52402c32bcbff3e0f518ef949b2d8
sanchirjav lkhaasuren <86226061+sanchirkkk@users.noreply.github.com>
2022-11-30 00:22:29 +0800

    s

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-app.use(express.static("public"));
-app.listen(process.env.PORT || 3000);
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: path.join(__dirname, "public") });
-});
-module.exports = app;
